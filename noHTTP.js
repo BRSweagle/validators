66564d026c1fea94dc30a8012654c56777e167e5
hostens <stefanhosten@hotmail.com>
2019-01-30 09:29:31 +0100

    Update noHTTP.js
    Added full path to keys failing validation

--- noHTTP.js
+++ noHTTP.js
@@ -10,24 +10,25 @@ var searchValue = "http:/";
 var errorFound = false;
 var errorMsg = "";
 
-function checkValue (mds) {
+function checkValue (mds, path) {
 
   for (var item in mds) {
+    path=path.concat('/' + item);
     // check if the key has a value or points to an object
     if  (typeof (mds[item]) === "object") {
       // if value is an object call recursively the function to search this subset
-      checkValue(mds[item]);
+      checkValue(mds[item], path);
     } else {
         // check if the value contains the given subvalue
         if  (mds[item].toLowerCase().includes(searchValue)) {
           errorFound = true;
-          errorMsg = errorMsg+"ERROR: key "+item+" contains an HTTP:// url value.\n";
+          errorMsg = errorMsg+"ERROR: key "+path+" contains an HTTP:// url value.\n";
           break;
         }
     }
   }
 }
 
-checkValue(metadataset);
+checkValue(metadataset, '');
 
 return {"result":!errorFound,"description":errorMsg};
