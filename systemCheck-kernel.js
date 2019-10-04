6374a7420b0e5d42b8d49579e8108da81d250359
Dimitris FINAS <dfinas@gmail.com>
2019-10-04 20:11:41 +0200

    add uname as backup check

--- systemCheck-kernel.js
+++ systemCheck-kernel.js
@@ -2,21 +2,29 @@
 
 // systemCheck-kernel.js
 // Creator:   Stefan/Dimitris for customer POC
-// Version:   1.0 - First
+// Version:   1.1 - Add uname key as backup approved list
 //
 var rootName = Object.keys(metadataset)[0];
 var root = metadataset[rootName];
-var approvedNode = root.approved.system.os.versions;
+var approvedNode1 = root.approved.system.os.versions;
+var approvedNode2 = root.approved.system.os.uname;
 var kernelPath = "system/os/uname";
+var approvedArray = [];
 
 var errorFound = false;
 var errors = [];
 var description = '';
 
-if (approvedNode == undefined) {
+if (approvedNode1 != undefined) {
+    // there is a list of approved versions, use it
+    approvedArray = Object.keys(approvedNode1);
+} else if (approvedNode2 != undefined) {
+    // there is no list of approved versions, use uname key instead
+    approvedArray.push(approvedNode2);
+} else {
+    // there is no list of approved versions and no uname key
     return {description: "*** ERROR: Approved list not found ", result: false};
 }
-var approvedArray = Object.keys(approvedNode);
 
 if (rootName == "servers") {
   // MDS is list of servers check all of them
@@ -33,7 +41,7 @@ description = errors.join(', ');
 return {description: description, result:!errorFound};
 
 function check(serverName, subset) {
-	var kernel = getValueByPath(subset, kernelPath);
+  var kernel = getValueByPath(subset, kernelPath);
   if (kernel == "ERROR: NOT FOUND") {
     errorFound = true;
     errors.push("*** For server ("+serverName+"), kernel version not found");
