2e05b14fd825412b3ac2c1dbe1aa13aed1969dc3
Dimitris FINAS <dfinas@gmail.com>
2019-10-23 18:36:12 +0200

    Add error array and path

--- noDevValue.js
+++ noDevValue.js
@@ -4,14 +4,15 @@
 // For each key in MDS, check if value contains a string identifying a DEV server
 // Creator:   Dimitris Finas for customer POC
 // Creator:   Use substringValidator.js as source
-// Version:   1.1 - add exception list
+// Version:   1.2 - add error array, max error display, and path mngt
 //
 
 // Define keywords in key values that defines a DEV value
 // like a DEV server, or DEBUG log level, or HTTP url instead of HTTPS
+// search for value is case insensitive
  var keyValuesWithDevValue = [
   "DEBUG",
-  "http:/"
+  "http:/",
 ];
 
 // List of keys that won't be checked
@@ -19,13 +20,26 @@ var exceptionList= [
     "KEYNAME"
     ];
 
-
-// searches is list of all key matching the rule
-// This will be used when validators allows custom error messages
-var searches = {};
-// errorFound is a local variable that founds errors
+// Definesthe max number of errors to return
+var includePath = false;
+var maxErrorDisplay = 5;
 var errorFound = false;
-var errorMsg = "";
+var errors = [];
+var description = '';
+
+// here we call our function with different search terms
+for (var i= 0; i < keyValuesWithDevValue.length; i++) {
+  if (includePath) { searchSubstringWithPath(metadataset, keyValuesWithDevValue[i].toLowerCase()) }
+  else { searchSubstring(metadataset, keyValuesWithDevValue[i].toLowerCase())};
+}
+
+if (errorFound) {
+  description = "Validation failed, only first "+maxErrorDisplay+" errors are displayed:" + errors.join(' ');
+} else {
+  description = "Validation passed successfully";
+}
+return {description: description, result:!errorFound};
+
 /**
  * searchSubsting function searches the whole metadataset to find keys that include a given substring
  *
@@ -33,8 +47,9 @@ var errorMsg = "";
  * searchValue must be the string we want check in the values
  */
 function searchSubstring (mds, searchValue) {
-
   for (var item in mds) {
+    // get out if we already reach max nb of errors to display
+    if (errors.length >= maxErrorDisplay) { break; }
     // check if the key has a value or points to an object
     if  (typeof (mds[item]) === "object") {
       // if value is an object call recursively the function to search this subset of the object
@@ -51,21 +66,40 @@ function searchSubstring (mds, searchValue) {
         }
         if (exception === false) {
           errorFound = true;
-          errorMsg = errorMsg+"ERROR: key "+item+" contains DEV value: "+mds[item]+".\n"
-          break;
+          errors.push("*** ERROR: key "+item+" contains DEV value: "+mds[item]);
         }
       }
     }
   }
 }
 
-// here we call our function with different search terms
-for(var i= 0; i < keyValuesWithDevValue.length; i++) {
-  searchSubstring(metadataset, keyValuesWithDevValue[i].toLowerCase());
-}
 
-if (errorFound) {
-  return {"result":!errorFound,"description":errorMsg};
-} else {
-  return {"result":!errorFound,"description":"Validation passed successfully"};
+function searchSubstringWithPath (mds, searchValue, prefix=[], level=0, pathSeparator="/") {
+  for (var item in mds) {
+    // get out if we already reach max nb of errors to display
+    if (errors.length >= maxErrorDisplay) { break; }
+    // check if the key has a value or points to an object
+    if  (typeof (mds[item]) === "object") {
+      // if value is an object call recursively the function to search this subset of the object
+      prefix[level] = item;
+      searchSubstringWithPath (mds[item], searchValue, prefix, level+1);
+    } else {
+      // check if the key contains the search term
+      if (mds[item].toLowerCase().includes(searchValue)) {
+        var exception = false;
+        for(var exc=0; exc < exceptionList.length; exc++) {
+          if (item.toLowerCase() === exceptionList[exc].toLowerCase()) {
+              exception=true;
+              break;
+          }
+        }
+        if (exception === false) {
+          errorFound = true;
+          var pre=prefix[0];
+          for (var i=1; i<level;i++) { pre = pre + pathSeparator + prefix[i]; }
+          errors.push("*** ERROR: key "+pre+pathSeparator+item+" contains DEV value: "+mds[item]);
+        }
+      }
+    }
+  }
 }
